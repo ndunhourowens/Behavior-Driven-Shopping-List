7995e222b61ff2a4ce9a33226e7c443907e6e54a
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-17 21:20:21 -1000

    set up app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
-// Create an instance of ShoppingList.
+var shopping_list = require('/shopping_list_item');
 
-// Invoke the shopping_list object's render() method, and store the output to a variable. Write the resulting output html into the content div. http://www.w3schools.com/jsref/prop_html_innerhtml.asp
+// Create an instance of ShoppingList.
+var groceryList = new ShoppingList();
+
+// Invoke the shopping_list object's render() method, and store the output
+//to a variable. Write the resulting output html into the
+//content div. http://www.w3schools.com/jsref/prop_html_innerhtml.asp
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "mother fucker worked!!!!!!!!!";
+}
+
+
 
 // Create an add_to_shopping_list function that will read the value of the title and description fields, then create a new variable named new_shopping_list_item that will store the result of constructing a new ShoppingListItem and passing in the values of title and description.
 
