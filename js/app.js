b07871fc97632c7ea2b85bc1e134cbc3bcb3e1a6
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-17 22:44:16 -1000

    testing how to use getElementById works

--- js/app.js
+++ js/app.js
@@ -7,11 +7,29 @@ var groceryList = new ShoppingList();
 //to a variable. Write the resulting output html into the
 //content div. http://www.w3schools.com/jsref/prop_html_innerhtml.asp
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "mother fucker worked!!!!!!!!!";
+function add_to_shopping_list() {
+  document.getElementById('add_shopping_list_item_button').innerHTML = "mother fucker worked!!!!!!!!!";
+
+
+}
+
+function wtf() {
+  // document.getElementById('content').innerHTML = "mother fucker worked!!!!!!!!!";
+  // adding to a list
+  document.getElementById('content').innerHTML = "piece of shit";
+}
+function wtf2() {
+  // document.getElementById('content').innerHTML = "mother fucker worked!!!!!!!!!";
+  // adding to a list
+  document.getElementById('content2').innerHTML = "piece of shit2";
 }
 
 
+function textField() {
+  // document.getElementById('content').innerHTML = "mother fucker worked!!!!!!!!!";
+  // adding to a list
+  document.getElementById('description').value = document,getElementById('title').value
+}
 
 // Create an add_to_shopping_list function that will read the value of the title and description fields, then create a new variable named new_shopping_list_item that will store the result of constructing a new ShoppingListItem and passing in the values of title and description.
 
