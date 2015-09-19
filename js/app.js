a510ab114b95fc60853d212e019a9712ed1dfd9e
Naomi Owens <ndunhourowens@Naomis-MacBook-Pro.local>
2015-09-18 15:47:22 -1000

    removed line 1 with require.  create var to invoke grocerylist render.  created new items to use add item to shopping list

--- js/app.js
+++ js/app.js
@@ -1,36 +1,30 @@
-var shopping_list = require('/shopping_list_item');
-
 // Create an instance of ShoppingList.
 var groceryList = new ShoppingList();
+var render = groceryList.render();
+document.getElementById("content").innerHTML = groceryList.render();
 
 // Invoke the shopping_list object's render() method, and store the output
 //to a variable. Write the resulting output html into the
 //content div. http://www.w3schools.com/jsref/prop_html_innerhtml.asp
 
 function add_to_shopping_list() {
-  document.getElementById('add_shopping_list_item_button').innerHTML = "mother fucker worked!!!!!!!!!";
+  var titleBox = document.getElementById("title").value;
+  var descBox = document.getElementById("description").value;
 
+  var item = new ShoppingListItem(titleBox,descBox);
+  groceryList.addItem(item);
+  console.log(titleBox);
+  console.log(descBox);
+  console.log(groceryList.items);
 
+/*=============================================================*
+| var textContent = document.getElementById("title").value;
+|  // alert(textContent);
+|
+| document.getElementById("daPlace").innerHTML = textContent;
+*=============================================================*/
 }
 
-function wtf() {
-  // document.getElementById('content').innerHTML = "mother fucker worked!!!!!!!!!";
-  // adding to a list
-  document.getElementById('content').innerHTML = "piece of shit";
-}
-function wtf2() {
-  // document.getElementById('content').innerHTML = "mother fucker worked!!!!!!!!!";
-  // adding to a list
-  document.getElementById('content2').innerHTML = "piece of shit2";
-}
-
-
-function textField() {
-  // document.getElementById('content').innerHTML = "mother fucker worked!!!!!!!!!";
-  // adding to a list
-  document.getElementById('content').value = document,getElementById('title').;
-
-}
 
 // Create an add_to_shopping_list function that will read the value of the title and description fields, then create a new variable named new_shopping_list_item that will store the result of constructing a new ShoppingListItem and passing in the values of title and description.
 
